99b7422391112cd2654174df0e33891415789e41
RafaelTalavera <rtalavera@live.com.ar>
2023-11-29 13:05:21 -0300

    initial commit add dependency

---
+++ src/main/java/com/axioma/backendpaymentapi/BackEndPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.axioma.backendpaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndPaymentApiApplication.class, args);
+    }
+
+}
